358daecb16eef1062790f558595c82b4e18503b1
NQ-edile <nqfreelance@gmail.com>
2024-08-06 23:06:46 +0200

    Implementing a 3-tier architecture using terraform

---
+++ terraform/vpc-architecture/nat-gateway.tf
@@ -0,0 +1,9 @@
+resource "aws_nat_gateway" "NQ-ngw" {
+  subnet_id = aws_subnet.NQ_subnet_pub_a.id
+}
+
+resource "aws_route" "NQ_route" {
+  route_table_id = aws_route_table.NQ_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.NQ-ngw.id
+}
