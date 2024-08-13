05d8fe75b734d55b825fe49c4cb1cf308d7d8878
NQ-edile <nqfreelance@gmail.com>
2024-08-13 14:05:09 +0200

    Created a bastion host

--- bastion-host/resources/ec2-instances.tf
+++ bastion-host/resources/ec2-instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "public_vm" {
     ami = "ami-0a2202cf4c36161a1"
     instance_type = "t3.micro"
-    security_groups = [aws_security_group.custom_sg.name]
+    vpc_security_group_ids = [aws_security_group.custom_sg.id]
     subnet_id = aws_subnet.public_sub.id
     key_name = aws_key_pair.key_pair.key_name
     associate_public_ip_address = true
@@ -14,7 +14,7 @@ resource "aws_instance" "public_vm" {
 resource "aws_instance" "private_vm" {
     ami = "ami-0a2202cf4c36161a1"
     instance_type = "t3.micro"
-    security_groups = [aws_security_group.custom_sg_private.name]
+    vpc_security_group_ids = [aws_security_group.custom_sg_private.id]
     subnet_id = aws_subnet.private_sub.id
     key_name = aws_key_pair.key_pair.key_name
     associate_public_ip_address = true
@@ -28,12 +28,10 @@ resource "aws_instance" "private_vm" {
 resource "aws_instance" "bastion_vm" {
     ami = "ami-0a2202cf4c36161a1"
     instance_type = "t3.micro"
-    security_groups = [aws_security_group.bastion_sg.name]
+    vpc_security_group_ids = [aws_security_group.bastion_sg.id]
     subnet_id = aws_subnet.public_sub.id
     key_name = aws_key_pair.key_pair.key_name
     associate_public_ip_address = true
-
-    depends_on = [aws_security_group.bastion_sg]
 
     tags = {
       Name = "nqs-bastion-instance"
--- bastion-host/resources/security-group.tf
+++ bastion-host/resources/security-group.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["${aws_instance.bastion_vm.public_ip}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
